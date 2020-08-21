025f5ead930b7720aae3c5a28be34df28b4a6305
Nguyen Duc Hanh <nguyenduchanhndh01091993@gmail.com>
2020-08-21 23:06:51 +0700

    add log model

--- model/Log.go
+++ model/Log.go
@@ -1,1 +1,14 @@
 package model
+
+import "time"
+
+type Log struct {
+	Node       string    `json:"node"`
+	Type       string    `json:"type"`
+	Code       int32     `json:"code"`
+	Module     string    `json:"module"`
+	Tstamp     string    `json:"tstamp"`
+	ShortText  string    `json:"shortText"`
+	Text       string    `json:"text"`
+	ServerTime time.Time `json:"serverTime"`
+}
